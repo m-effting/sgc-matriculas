b313bc7e64be7bfb13bac068e5f1a99584637d75
m-effting <99153192+m-effting@users.noreply.github.com>
2026-01-27 07:59:19 -0300

    preparar app para render deploy

--- src/js/modules/supabase.js
+++ src/js/modules/supabase.js
@@ -1,9 +1,9 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+   import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
-const SUPABASE_URL = 'https://atdgxqipsfflemfdkydr.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_fa2wQrQs8SSznK1e_eGi1g_5JSWkzJ_';
+   const SUPABASE_URL = 'https://atdgxqipsfflemfdkydr.supabase.co';
+   const SUPABASE_KEY = 'sb_publishable_fa2wQrQs8SSznK1e_eGi1g_5JSWkzJ_';
 
-/**
- * Instância global do cliente Supabase para comunicação com o banco de dados.
- */
-export const db = createClient(SUPABASE_URL, SUPABASE_KEY);
+   /**
+    * Instância global do cliente Supabase para comunicação com o banco de dados.
+    */
+   export const db = createClient(SUPABASE_URL, SUPABASE_KEY);
